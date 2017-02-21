bba600e38026b35b0cdd39d1cd86f9690ca43975
yangning <yangning@vodjk.com>
2017-02-21 13:30:46 +0800

    Initial commit

---
+++ Pod/Classes/WXImageCompress.swift
@@ -0,0 +1,99 @@
+//
+//  Luban.swift
+//  XMImagePicker
+//
+//  Created by tiger on 2017/2/16.
+//  Copyright © 2017年 xinma. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+enum WechatCompressType {
+    case session
+    case timeline
+}
+
+extension UIImage {
+    
+    /**
+     wechat image compress
+     
+     - parameter type: session image boundary is 800, timeline is 1280
+     
+     - returns: thumb image
+     */
+    func wxCompress(type: WechatCompressType = .timeline) -> UIImage {
+        let size = self.wxImageSize(type: type)
+        let reImage = resizedImage(size: size)
+        let data = UIImageJPEGRepresentation(reImage, 0.5)!
+        return UIImage.init(data: data)!
+    }
+    
+    /**
+     get wechat compress image size
+     
+     - parameter type: session  / timeline
+     
+     - returns: size
+     */
+    private func wxImageSize(type: WechatCompressType) -> CGSize {
+        var width = self.size.width
+        var height = self.size.height
+        
+        var boundary: CGFloat = 1280
+        
+        // width, height <= 1280, Size remains the same
+        guard width > boundary || height > boundary else {
+            return CGSize(width: width, height: height)
+        }
+        
+        // aspect ratio
+        let s = max(width, height) / min(width, height)
+        if s <= 2 {
+            // Set the larger value to the boundary, the smaller the value of the compression
+            let x = max(width, height) / boundary
+            if width > height {
+                width = boundary
+                height = height / x
+            } else {
+                height = boundary
+                width = width / x
+            }
+        } else {
+            // width, height > 1280
+            if min(width, height) >= boundary {
+                boundary = type == .session ? 800 : 1280
+                // Set the smaller value to the boundary, and the larger value is compressed
+                let x = min(width, height) / boundary
+                if width < height {
+                    width = boundary
+                    height = height / x
+                } else {
+                    height = boundary
+                    width = width / x
+                }
+            }
+        }
+        return CGSize(width: width, height: height)
+    }
+    
+    /**
+     Zoom the picture to the specified size
+     
+     - parameter newSize: image size
+     
+     - returns: new image
+     */
+    private func resizedImage(size: CGSize) -> UIImage {
+        let newRect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        var newImage: UIImage!
+        UIGraphicsBeginImageContext(newRect.size)
+        newImage = UIImage(cgImage: self.cgImage!, scale: 1, orientation: self.imageOrientation)
+        newImage.draw(in: newRect)
+        newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+}
---
+++ WXImageCompress.podspec
@@ -0,0 +1,12 @@
+Pod::Spec.new do |s|
+  s.name             = 'WXImageCompress'
+  s.version          = '0.1.0'
+  s.summary          = 'Image size quality compression, very close to WeChat picture compression strategy'
+  s.homepage         = 'https://github.com/hucool/WXImageCompress'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' }
+  s.author           = { 'tiger' => 'tiger@socool@gmail.com' }
+  s.source           = { :git => 'https://github.com/hucool/WXImageCompress.git', :tag => s.version.to_s }
+  s.ios.deployment_target = '8.0'
+  s.source_files = 'Pod/Classes/**/*'
+  s.frameworks = 'UIKit', 'Foundation'
+end
